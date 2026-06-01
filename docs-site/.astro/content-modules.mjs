693c6814465e3e815e648600413673c53c18d858
SinishaDjukic <s.djukic@gmail.com>
2026-06-01 15:20:09 +0300

    chore(docs-site): gitignore wrangler local-dev state cache
    The .wrangler/ directory is miniflare's local emulation cache (KV,
    cache, D1, etc.). Machine-local; should never be committed.

---
+++ docs-site/.astro/content-modules.mjs
@@ -0,0 +1,7 @@
+
+export default new Map([
+["src/content/docs/concepts/lifecycle-and-state.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fconcepts%2Flifecycle-and-state.mdx&astroContentModuleFlag=true")],
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/introduction/how-it-works.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fintroduction%2Fhow-it-works.mdx&astroContentModuleFlag=true")],
+["src/content/docs/introduction/watch.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fintroduction%2Fwatch.mdx&astroContentModuleFlag=true")]]);
+		
